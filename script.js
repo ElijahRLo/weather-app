622b51f9e6768cb01c7032909f077595bcb27e84
ElijahRLo <loelijah2002@gmail.com>
2022-08-07 13:38:02 -1000

    Updates weather information properly

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let weather = {
     "apiKey" : "9ab82efaaa04a155296bc934c8a5d1a2",
-    fetchWeather: function () {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=champaign&units=imperial&appid=9ab82efaaa04a155296bc934c8a5d1a2"
+
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
         ).then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => this.displayWeather(data))
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+
+
+
+    
     }
 }

f0123f2ac543ca909e5d6b1f06c712c360051a0a
ElijahRLo <loelijah2002@gmail.com>
2022-08-07 13:48:01 -1000

    Feels like functionality

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ let weather = {
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, feels_like, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        console.log(name, icon, description, temp, feels_like, humidity, speed);
 
 
 
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°";
+        document.querySelector(".description").innerText = "Condition: " + description;
+        document.querySelector(".feels").innerText = "Feels like: " + feels_like + "°F";
+        document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
 
